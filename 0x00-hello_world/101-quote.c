27128e91ba5e08086d4c7c9b39644dbbf831deb1
Fafagrace <akushika@gmail.com>
2023-02-17 01:12:18 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 #include <unistd.h>
 /**
  * main - this is a main function in c
- * Return: wnd with 1 if successul
+ * Return: end with 1 if successul
  */
 int main(void)
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
