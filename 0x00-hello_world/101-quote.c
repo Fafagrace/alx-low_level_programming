f6d130e6b5844ea6c583c392b0281b33d9eedcea
Fafagrace <akushika@gmail.com>
2023-02-16 16:20:23 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - main function
- * Return 1 (Success)
+ * main - this is a main function in c
+ * Return: wnd with 1 if successul
  */
 int main(void)
-{
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+return (1);
 }
