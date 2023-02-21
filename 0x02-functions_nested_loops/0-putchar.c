3052879e62114e565592248bb56fbbd9ddea77a2
Fafagrace <akushika@gmail.com>
2023-02-21 11:44:04 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - as a function
+ * _putchar: print putchar alphabetss
+ * Return: end with 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
