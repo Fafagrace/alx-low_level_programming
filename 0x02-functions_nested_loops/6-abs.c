7e0788d918efa5abc6cdc5a0043e7f48976f8fcc
Fafagrace <akushika@gmail.com>
2023-02-22 07:19:22 -0800

    6-main

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - checks for the absolute value
+ * @n: the int to check
+ * Return: end with n else -n
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
